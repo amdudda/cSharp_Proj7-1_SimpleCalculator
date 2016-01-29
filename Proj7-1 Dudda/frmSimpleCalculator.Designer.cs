ac91837e3f13fb72e6c1eaf8cafa9270b0846b9b
amdudda <tk0654wm@minneapolis.edu>
2016-01-28 19:01:52 -0600

    Initial Commit

---
+++ Proj7-1 Dudda/frmSimpleCalculator.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Proj7_1_Dudda
+{
+    partial class frmSimpleCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtOperand1 = new System.Windows.Forms.TextBox();
+            this.txtOperator = new System.Windows.Forms.TextBox();
+            this.txtOperand2 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A. M. Dudda - Project 7-1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Operand 1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Operator:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Operand 2:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Result:";
+            // 
+            // txtOperand1
+            // 
+            this.txtOperand1.Location = new System.Drawing.Point(84, 32);
+            this.txtOperand1.Name = "txtOperand1";
+            this.txtOperand1.Size = new System.Drawing.Size(57, 20);
+            this.txtOperand1.TabIndex = 5;
+            this.txtOperand1.TextChanged += new System.EventHandler(this.InputBox_TextChanged);
+            // 
+            // txtOperator
+            // 
+            this.txtOperator.Location = new System.Drawing.Point(84, 56);
+            this.txtOperator.Name = "txtOperator";
+            this.txtOperator.Size = new System.Drawing.Size(25, 20);
+            this.txtOperator.TabIndex = 6;
+            this.txtOperator.TextChanged += new System.EventHandler(this.InputBox_TextChanged);
+            // 
+            // txtOperand2
+            // 
+            this.txtOperand2.Location = new System.Drawing.Point(84, 82);
+            this.txtOperand2.Name = "txtOperand2";
+            this.txtOperand2.Size = new System.Drawing.Size(57, 20);
+            this.txtOperand2.TabIndex = 7;
+            this.txtOperand2.TextChanged += new System.EventHandler(this.InputBox_TextChanged);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(84, 107);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 8;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCalculate.Location = new System.Drawing.Point(10, 141);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 9;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(108, 141);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmSimpleCalculator
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(205, 189);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtOperand2);
+            this.Controls.Add(this.txtOperator);
+            this.Controls.Add(this.txtOperand1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmSimpleCalculator";
+            this.Text = "Simple Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtOperand1;
+        private System.Windows.Forms.TextBox txtOperator;
+        private System.Windows.Forms.TextBox txtOperand2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Proj7-1 Dudda/frmSimpleCalculator.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proj7_1_Dudda
+{
+    public partial class frmSimpleCalculator : Form
+    {
+        public frmSimpleCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // validation needed before we let the program continue
+            if (HasValidInput())
+            {
+                // if validation is successful, go ahead and do the math
+                decimal result = Calculate();
+                // then we output the result with four decimals.
+                txtResult.Text = result.ToString("n4");
+            }
+        }
+
+        // clear the box when text boxes are changed
+        private void InputBox_TextChanged(object sender, EventArgs e)
+        {
+            txtResult.Text = "";
+        }
+
+private bool HasValidInput()
+{
+            if (!ValidateOperand(txtOperand1.Text))
+            {
+                MessageBox.Show("Please enter a number between 0 and 1,000,000 for Operand 1.");
+                return false;
+            }
+            else if (!ValidateOperand(txtOperand2.Text))
+            {
+                MessageBox.Show("Please enter a number between 0 and 1,000,000 for Operand 2.");
+                return false;
+            }
+            else if (!IsOperator(txtOperator.Text))
+            {
+                MessageBox.Show("Please enter a valid operator.  It must be one of:\n + - * /");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+}
+
+        private decimal Calculate()
+        {
+            // for now, just make sure we can get a result
+            decimal operand1 = decimal.Parse(txtOperand1.Text);
+            decimal operand2 = decimal.Parse(txtOperand2.Text);
+            string op1 = txtOperator.Text;
+            decimal result = 0m;
+           
+            switch (op1)
+            {
+                case "+":
+                    result = operand1 + operand2;
+                    break;
+                case "-":
+                    result = operand1 - operand2;
+                    break;
+                case "*":
+                    result = operand1 * operand2;
+                    break;
+                case "/":
+                    result = operand1 / operand2;
+                    break;
+                default:
+                    // do nothing, we didn't get a valid operator
+                    break;
+            }
+            return result;
+        }
+
+
+        // validate the operands
+        private bool ValidateOperand(string oper)
+        {
+            return IsPresent(oper) && IsDecimal(oper) && IsValidData(oper);
+        }
+
+        // validation methods for operands
+        private bool IsPresent(string toCheck)
+        {
+            return toCheck != "";
+        }
+
+        private bool IsDecimal(string toCheck)
+        {
+            decimal whatevs;  // disposable local variable to keep code from breaking
+            if (decimal.TryParse(toCheck, out whatevs))
+            { 
+                return true; 
+            }
+            else { return false; }
+        }
+
+        private bool IsValidData(string toCheck)
+        {
+            decimal someNumber = decimal.Parse(toCheck);
+            if (someNumber <= 0 || someNumber >= 1000000) 
+            {
+                return false;
+            }
+            else { return true; }
+        }
+
+        // validate the operator
+        private bool IsOperator(string symbol)
+        {
+            return symbol == "+" || symbol == "-" || symbol == "*" || symbol == "/";
+        }
+
+        
+    } // end frmSimpleCalculator
+}
